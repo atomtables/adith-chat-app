483be14c9e0acdfa9e30c0ca5527ff4587d780b9
adithv08 <adithv08@gmail.com>
2022-12-15 16:50:48 -0500

    rename

--- adith_chat_app/adith_chat_app.py
+++ adith_chat_app/adith_chat_app.py
@@ -3,13 +3,15 @@ import hashlib
 
 
 class State(pc.State):
-    my_string: str = "not activate"
-
-    def increment(self):
-        if self.my_string == "not activate":
-            self.my_string = "activate"
-        else:
-            self.my_string = "not activate"
+    log_in = False
+    username = ""
+    password = ""
+    def flip_log_in(self):
+        self.log_in = not self.log_in
+    def set_user(self, text):
+        self.username = text
+    def set_pass(self, text):
+        self.password = hashlib.sha256(text)
 
 
 def index():
@@ -20,17 +22,20 @@ def index():
                 pc.input(
                     placeholder="Username",
                     width="100%",
+                    on_blur=State.set_user,
                 ),
                 pc.input(
                     placeholder="Password",
                     type_="password",
-                    width="100%"
+                    width="100%",
+                    on_blur=State.set_pass,
                 ),
                 width="100%"
             ),
             pc.button(
                 "log in",
                 width="100%",
+                on_click=State.flip_log_in,
             ),
             pc.divider(),
             bg="white",
@@ -41,7 +46,7 @@ def index():
         ),
         width="100%",
         height="100vh",
-        background="radial-gradient(circle at 22% 11%,rgba(62, 180, 137,.20),hsla(0,0%,100%,0) 19%),radial-gradient(circle at 82% 25%,rgba(33,150,243,.18),hsla(0,0%,100%,0) 35%),radial-gradient(circle at 25% 61%,rgba(250, 128, 114, .28),hsla(0,0%,100%,0) 55%)",
+        background="radial-gradient(circle at 22% 11%,rgba(150, 175, 217,0.25),hsla(0,0%,100%,0) 19%),radial-gradient(circle at 82% 25%,rgba(174, 242, 232,1),hsla(0,0%,100%,0) 35%),radial-gradient(circle at 25% 61%,rgba(209, 237, 202, 1),hsla(0,0%,100%,0) 55%)",
     )
 
 
--- pcconfig.py
+++ pcconfig.py
@@ -2,7 +2,7 @@ import pynecone as pc
 
 
 config = pc.Config(
-    app_name="pynecone_test_stuff",
+    app_name="adith_chat_app",
     bun_path="$HOME/.bun/bin/bun",
     db_url="sqlite:///pynecone.db",
     env=pc.Env.DEV,
