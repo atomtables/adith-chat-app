a127c42e062ec67c860eef07c4a09171e4b02295
adithv08 <adithv08@gmail.com>
2022-12-27 10:52:25 -0500

    aligning core

--- pcconfig.py
+++ pcconfig.py
@@ -2,7 +2,7 @@ import pynecone as pc
 
 
 config = pc.Config(
-    app_name="adith_chat_app",
+    app_name="atom-chat-online-pyne",
     bun_path="$HOME/.bun/bin/bun",
     db_url="sqlite:///pynecone.db",
     env=pc.Env.DEV,
